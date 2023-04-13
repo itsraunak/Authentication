e0e539a35d766e3d5adcd844e9f6cf50dd6561bd
Raunak Kumar <58897751+itsraunak@users.noreply.github.com>
2023-04-13 10:31:00 +0530

    Project is Conneced to the Online DB

---
+++ config/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MongoURI: "mongodb+srv://itisraunak:W20PfrEJzOAuy9BW@auth.uyajbvz.mongodb.net/auth"
+    // MongoURI: "mongodb://localhost:27017/auth"
+}
+
+
+// W20PfrEJzOAuy9BW
